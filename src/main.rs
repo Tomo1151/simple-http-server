b271af06f3e9cc0f516477b09a1d85905dfd9a5e
Tomo <94550718+Tomo1151@users.noreply.github.com>
2026-05-19 04:01:47 +0900

    ポートが使用中の時のエラーハンドリングを追加

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::{io::Read, net::{TcpListener, TcpStream}};
+use std::{io::{self, Read}, net::{SocketAddr, TcpListener, TcpStream}};
 
 fn main() {
-    // リッスンするアドレスの定義
-    let address = "127.0.0.1";
-    let port = "1151";
-
-    // TCPリスナーを作成し，指定アドレスをリッスン
-    let listener = TcpListener::bind(format!("{}:{}", address, port)).unwrap();
+    const PORT: u16 = 1151;
+    let listener = match bind_available_port(PORT) {
+        Ok(listener) => listener,
+        Err(_) => panic!("Failed to create TCP listener."),
+    };
 
     // ストリームからメッセージを表示
     for stream in listener.incoming() {
@@ -25,3 +24,34 @@ fn handle_connection_stream(mut stream: TcpStream) {
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
+
+// 使用できるポートでTCPリスナーを作成する関数
+fn bind_available_port(initial_port: u16) -> Result<TcpListener, io::Error> {
+    const MAX_TRIES: u16 = 10;
+
+    // 使用できるポートが見つかるまで試行
+    for i in 0..MAX_TRIES {
+        let port = initial_port + i;
+        let address = SocketAddr::from(([127, 0, 0, 1], port));
+        
+        match TcpListener::bind(address) {
+            Ok(listener) => {
+                println!("🎉 TCP connection listening on port {} !", port);
+                return Ok(listener);
+            },
+            Err(e) => {
+                if e.kind() == io::ErrorKind::AddrInUse {
+                    continue;
+                }
+                return Err(e);
+            }
+        }
+    }
+
+    // 全てのポートが使用中の場合はエラーを返す
+    Err(io::Error::new(
+        io::ErrorKind::AddrInUse,
+        "Failed to bind TCP listener because of port in use."
+    ))
+}
+
